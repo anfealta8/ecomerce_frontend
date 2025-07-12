03d858665e09cdb209734f19039056d10a55017e
ANDRES TALERO <andres_alvarado@cun.edu.co>
2025-07-12 09:59:13 -0500

    feat: Basic frontend structure with routing and components

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,17 +1,16 @@
-// src/app/shared/header/header.component.ts
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router'; // <-- Importa estos
+import { RouterLink, RouterLinkActive } from '@angular/router'; 
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
-    RouterLink,         // <-- Añade RouterLink
-    RouterLinkActive    // <-- Añade RouterLinkActive
+    RouterLink,         
+    RouterLinkActive    
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-  // ...
+  
 }
